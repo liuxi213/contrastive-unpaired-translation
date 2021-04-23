f9e48be9ce92f1a58814f94128f07fd172e68252
Xi Liu <Xi.Liu@intusurg.com>
2021-04-23 12:20:08 -0400

    add inference

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
 python train.py --dataroot /mnt/data/grumpifycat --name grumpycat_FastCUT --CUT_mode FastCUT
+python test.py --dataroot /mnt/data/grumpifycat --name grumpycat_FastCUT --CUT_mode FastCUT --phase train
