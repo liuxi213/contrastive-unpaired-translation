5d7f7241b760b5821057ae8c775d7359b03222e0
Xi Liu <Xi.Liu@intusurg.com>
2021-04-26 11:52:11 -0400

    5 epochs

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-python train.py --dataroot /mnt/data/grumpifycat --name grumpycat_FastCUT --CUT_mode FastCUT --n_epochs 10
+python train.py --dataroot /mnt/data/grumpifycat --name grumpycat_FastCUT --CUT_mode FastCUT --n_epochs 5
 python test.py --dataroot /mnt/data/grumpifycat --name grumpycat_FastCUT --CUT_mode FastCUT --phase train
