030de8247c2fdd337ee6710a0686344c4297d07a
liuxi213 <xi.liu@intusurg.com>
2021-05-26 18:23:43 +0000

    modify for cnvfrg

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-python train.py --dataroot /mnt/data/grumpifycat --name grumpycat_FastCUT --CUT_mode FastCUT --n_epochs 5
-python test.py --dataroot /mnt/data/grumpifycat --name grumpycat_FastCUT --CUT_mode FastCUT --phase train
+python train.py --dataroot /data/cat --name grumpycat_FastCUT --CUT_mode FastCUT --n_epochs 5
+python test.py --dataroot /data/cat --name grumpycat_FastCUT --CUT_mode FastCUT --phase train
